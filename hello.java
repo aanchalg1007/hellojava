c677fe49506d03ef3e672093583bdac6c997d07f
aanchalg1007 <107946012+aanchalg1007@users.noreply.github.com>
2026-03-02 14:16:58 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,7 @@
 public class hello{
     public static void main(String[]args){
-        System.out.println("Hello World");
+        System.out.println("Hello");
+          System.out.println("World");
         System.out.println("Addition :"+ (2 + 3));
         System.out.println("Addition :"+ (5 + 3));
 
